0893436e2d8b67ed6419362db43ae5ff6573c1c6
Streaming <Streaming@IYC3EPDT8.iyc.ishafoundation.org>
2019-07-14 09:02:04 +0530

    remap and srt for 2 channels

--- scripts/remap.sh
+++ scripts/remap.sh
@@ -16,7 +16,7 @@ case $1 in
 2)
 while true
 do
-/usr/bin/ffmpeg -re -i rtmp://127.0.0.1/main/stream1080 -filter_complex "[0:a]pan=mono|c0=c0,aresample=async=1000[a0];[0:a]pan=mono|c0=c1,aresample=async=1000[a1];[0:a]pan=mono|c0=c2,aresample=async=1000[a2]" -map 0:v -map [a0] -vcodec copy -acodec aac -ab 128k -f flv -strict -2 rtmp://127.0.0.1/main/stream1 -map 0:v -map [a1] -vcodec copy -acodec aac -ab 128k -f flv -strict -2 rtmp://127.0.0.1/main/stream2
+/usr/bin/ffmpeg -re -i rtmp://127.0.0.1/main/stream1080 -filter_complex "[0:a]pan=mono|c0=c0,aresample=async=1000[a0];[0:a]pan=mono|c0=c1,aresample=async=1000[a1]" -map 0:v -map [a0] -vcodec copy -acodec aac -ab 128k -f flv -strict -2 rtmp://127.0.0.1/main/stream1 -map 0:v -map [a1] -vcodec copy -acodec aac -ab 128k -f flv -strict -2 rtmp://127.0.0.1/main/stream2
 echo "Restarting remapping..."
 sleep .2
 done
--- scripts/srtaccept.sh
+++ scripts/srtaccept.sh
@@ -20,9 +20,7 @@ exit 0
 *)
 while true
 do
-#/root/ffmpeg_sources/srt/build/srt-live-transmit "srt://:9000" "file://con" | /usr/bin/ffmpeg -re -i pipe:0 -map 0:0 -map 0:1 -vcodec libx264 -preset veryfast -profile:v high -acodec aac -f flv -strict -2 rtmp://127.0.0.1/main/stream1080
-#/root/ffmpeg_sources/srt/build/srt-live-transmit "srt://:9000" "file://con" | /usr/bin/ffmpeg -re -i pipe:0 -map 0 -acodec copy -vcodec libx264 -preset ultrafast -profile:v high -s 1280x720 -f flv - | /usr/bin/ffmpeg -re -f flv -i - -map 0:0 -map 0:1 -vcodec copy -acodec aac -strict -2 rtmp://127.0.0.1/main/stream1 -map 0:0 -map 0:2 -vcodec copy -acodec aac -f flv -strict -2 rtmp://127.0.0.1/main/stream2
-/root/ffmpeg_sources/srt/build/srt-live-transmit "srt://:9000" "file://con" | /usr/bin/ffmpeg -re -i pipe:0 -filter_complex "[0:1]pan=mono|c0=c0,aresample=async=1000[a1];[0:1]pan=mono|c0=c1,aresample=async=1000[a2];[0:2]pan=mono|c0=c0,aresample=async=1000[a3];[0:2]pan=mono|c0=c1,aresample=async=1000[a4];[0:3]pan=mono|c0=c0,aresample=async=1000[a5];[0:3]pan=mono|c0=c1,aresample=async=1000[a6];[0:4]pan=mono|c0=c0,aresample=async=1000[a7];[0:4]pan=mono|c0=c1,aresample=async=1000[a8];[0:5]pan=mono|c0=c0,aresample=async=1000[a9];[0:5]pan=mono|c0=c1,aresample=async=1000[a10]" -vcodec copy -map 0:0 -map [a1] -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream1 -map 0:0 -map [a2] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream2 -map 0:0 -map [a3] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream3 -map 0:0 -map [a4] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream4 -map 0:0 -map [a5] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream5 -map 0:0 -map [a6] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream6 -map 0:0 -map [a7] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream7 -map 0:0 -map [a8] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream8 -map 0:0 -map [a9] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream9 -map 0:0 -map [a10] -vcodec copy -acodec aac -strict -2 -f flv rtmp://127.0.0.1/main/stream10
+~/ffmpeg_sources/srt/build/srt-live-transmit "srt://:9000" "file://con" | /usr/bin/ffmpeg -re -i pipe:0 -map 0:0 -map 0:1 -vcodec libx264 -preset veryfast -profile:v high -acodec aac -f flv -strict -2 rtmp://127.0.0.1/main/stream1080
 echo "Restarting SRT Accept..."
 sleep .2
 done
@@ -40,5 +38,3 @@ echo "Turning off SRT Accept"
 echo "SRT Accept is already on"
 esac
 fi
-
-
