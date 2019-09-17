cdb45b9fc63ce87fac9abdc6860703c60b6ad4bf
Streaming <Streaming@IYC3EPDT8.iyc.ishafoundation.org>
2019-09-17 13:00:09 +0530

    Settings page. Unworking upload form.

--- html/config.php
+++ html/config.php
@@ -48,4 +48,38 @@ echo "<br>";
 $output = shell_exec("sudo /bin/bash /usr/local/nginx/scripts/adddestination.sh \"$rtmp_url\" \"$stream_id\" \"$output_id\" \"$resolution\" $name_id");
 echo $output;
 }
+
+if (isset($_GET['upload'])) {
+	$target_dir = "../scripts/images/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 20000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "mp4") {
+    echo "Sorry, only mp4 file is allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+}
 ?>
