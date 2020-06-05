58f83ac879f1494bf6a0ece4d8b546ceadf39c7a
Streaming <Streaming@IYC3EPDT8.iyc.ishafoundation.org>
2020-06-05 12:39:08 +0530

    v1 of mobile, non-flash player - jsmpeg

---
+++ html/player.php
@@ -0,0 +1,47 @@
+
+<?php
+
+if (isset($_GET['1on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 1 on");echo $output;}
+if (isset($_GET['2on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 2 on");echo $output;}
+if (isset($_GET['3on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 3 on");echo $output;}
+if (isset($_GET['4on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 4 on");echo $output;}
+if (isset($_GET['5on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 5 on");echo $output;}
+if (isset($_GET['6on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 6 on");echo $output;}
+if (isset($_GET['7on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 7 on");echo $output;}
+if (isset($_GET['8on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 8 on");echo $output;}
+if (isset($_GET['9on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 9 on");echo $output;}
+if (isset($_GET['10on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 10 on");echo $output;}
+if (isset($_GET['11on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 11 on");echo $output;}
+if (isset($_GET['12on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 12 on");echo $output;}
+if (isset($_GET['13on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 13 on");echo $output;}
+if (isset($_GET['14on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 14 on");echo $output;}
+if (isset($_GET['15on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 15 on");echo $output;}
+if (isset($_GET['16on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 16 on");echo $output;}
+if (isset($_GET['17on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 17 on");echo $output;}
+if (isset($_GET['18on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 18 on");echo $output;}
+if (isset($_GET['19on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 19 on");echo $output;}
+if (isset($_GET['20on'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 20 on");echo $output;}
+
+if (isset($_GET['1off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 1 off");echo $output;}
+if (isset($_GET['2off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 2 off");echo $output;}
+if (isset($_GET['3off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 3 off");echo $output;}
+if (isset($_GET['4off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 4 off");echo $output;}
+if (isset($_GET['5off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 5 off");echo $output;}
+if (isset($_GET['6off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 6 off");echo $output;}
+if (isset($_GET['7off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 7 off");echo $output;}
+if (isset($_GET['8off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 8 off");echo $output;}
+if (isset($_GET['9off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 9 off");echo $output;}
+if (isset($_GET['10off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 10 off");echo $output;}
+if (isset($_GET['11off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 11 off");echo $output;}
+if (isset($_GET['12off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 12 off");echo $output;}
+if (isset($_GET['13off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 13 off");echo $output;}
+if (isset($_GET['14off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 14 off");echo $output;}
+if (isset($_GET['15off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 15 off");echo $output;}
+if (isset($_GET['16off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 16 off");echo $output;}
+if (isset($_GET['17off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 17 off");echo $output;}
+if (isset($_GET['18off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 18 off");echo $output;}
+if (isset($_GET['19off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 19 off");echo $output;}
+if (isset($_GET['20off'])) {$output = exec("sudo /bin/bash /usr/local/nginx/scripts/player.sh 20 off");echo $output;}
+
+?>
+
---
+++ scripts/player.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+streamid="stream"$1;
+ffscreen="fftots"$1;
+
+case $2 in
+on)
+if [ $(ps aux | grep "wstoff" | awk '{print $2}' | wc -l) -eq 1 ]; then
+screen -dm -S wstoff
+screen -S wstoff -p 0 -X stuff 'node /usr/local/nginx/html/websocket-relay.js supersecret 8031 443\n';
+fi
+
+LCK="/usr/local/nginx/scripts/tmp/${streamid}.LCK";
+exec 8>$LCK;
+
+if flock -n -x 8; then
+if [ -z "$STY" ]; then
+kill $(ps aux | grep "fftots" | awk '{print $2}')
+echo "Turning on $streamid player"
+#sleep .2
+exec screen -dm -S $ffscreen /bin/bash "$0" "$1" "$2"
+fi
+
+while true
+do
+ffmpeg -i rtmp://127.0.0.1/main/$streamid -f mpegts -acodec mp2 -ar 44100 -ac 1 -b:a 96k -vcodec mpeg1video -r 25 -bf 0 -s 320x180 -b:v 200k -muxdelay 0.001 http://localhost:8031/supersecret
+echo "Restarting ffmpeg..."
+sleep .2
+done
+
+else
+echo $streamid " player is already on"
+fi
+;;
+
+off)
+if [ $(ps aux | grep $ffscreen | awk '{print $2}' | wc -l) -gt 0 ]; then
+kill $(ps aux | grep $ffscreen | awk '{print $2}')
+fi
+
+#if [ $(ps aux | grep "wstoff" | awk '{print $2}' | wc -l) -gt 0 ]; then
+#kill $(ps aux | grep "wstoff" | awk '{print $2}')
+#fi
+echo "Turning off $streamid player"
+
+esac
