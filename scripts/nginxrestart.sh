f425f234584a84b52367ec0c8606bdcd096e7dff
Streaming <Streaming@IYC3EPDT8.iyc.ishafoundation.org>
2021-05-06 16:46:42 +0530

    Comments in nginrestart

--- scripts/nginxrestart.sh
+++ scripts/nginxrestart.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Deletes NGINX logs on restart to conserve space
 sudo rm /usr/local/nginx/logs/*.log
+
+# Resets any lowerthirds applied previously to blank images
 sudo cp /usr/local/nginx/scripts/images/lowerthird/*lowerthird.png /usr/local/nginx/scripts/images
-sudo /usr/local/nginx/sbin/nginx -s stop; sudo /usr/local/nginx/sbin/nginx
+
+# Replaces page titles in webpages to name of instance
 sudo sed -i "s|<title>.*</title>|<title>$(hostname)</title>|" /usr/local/nginx/html/control.html
 sudo sed -i "s|<p align="\""center"\"" id="\""server-name"\""><b>.*</b></p>|<p align="\""center"\"" id="\""server-name"\""><b>$(hostname)</b></p>|" /usr/local/nginx/html/control.html
 sudo sed -i "s|<title>.*</title>|<title>$(hostname) Settings</title>|" /usr/local/nginx/html/settings.html
 sudo sed -i "s|<title>.*</title>|<title>$(hostname) Stats</title>|" /usr/local/nginx/html/stat.xsl
+
+# Restart NGINX
+sudo /usr/local/nginx/sbin/nginx -s stop; sudo /usr/local/nginx/sbin/nginx
